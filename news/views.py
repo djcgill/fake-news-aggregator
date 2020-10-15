f2d1f82b399c696ceb1f3892ff4e01f4583e63e4
djcg <daniel_gilliam@hotmail.com>
2020-10-15 21:08:22 +0100

    Joke

--- news/views.py
+++ news/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("This is the index for news")
+    return HttpResponse("DONT JUDGE")
