7d76c30d5d9eaf1d35b96662c43d3c5450b8924c
djcg <daniel_gilliam@hotmail.com>
2020-11-30 20:54:01 +0000

    Added logging config to settings

--- fake_news_aggregator/settings.py
+++ fake_news_aggregator/settings.py
@@ -127,6 +127,20 @@ STATICFILES_DIRS = [
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
+
 try:
     import local_settings
 except ImportError:
